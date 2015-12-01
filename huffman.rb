5bac2cca94c85c40740db9cc3e2dc93839463d92
nathanielltaylor <ntaylor390@gmail.com>
2015-11-30 19:11:50 -0500

    frequencies refactor

--- huffman.rb
+++ huffman.rb
@@ -1,12 +1,19 @@
 require_relative 'huffman_nodes'
 require 'pry'
 
+def get_frequencies(char_array, char_freqs)
+  char_array.each do |char|
+    char_freqs[char] = 0 unless char_freqs.has_key?(char)
+    char_freqs[char] += 1
+  end
+end
+
 def build_huffman_tree(input_array, character_freq)
   q = []
   accounted_for = []
   for i in 0..(input_array.length - 1)
     if !accounted_for.include?(input_array[i])
-      new_node = HuffmanNode.new(input_array[i], (character_freq[input_array[i]].to_f / input_array.length))
+      new_node = HuffmanNode.new(input_array[i], character_freq[input_array[i]])
       q << new_node
       accounted_for << input_array[i]
     end
@@ -39,17 +46,23 @@ end
 def huffman_compress(text)
   char_array = text.split('')
   char_freqs = {}
-  char_array.each do |char|
-    char_freqs[char] = 0 unless char_freqs.has_key?(char)
-    char_freqs[char] += 1
-  end
+  get_frequencies(char_array, char_freqs)
   first_node = build_huffman_tree(char_array, char_freqs)
   code_index = {}
   tree_traversal(first_node, code_index)
   compressed = ""
   char_array.each { |char| compressed << code_index[char] }
-  return compressed
+  compressed
+end
+
+def huffman_decompress(code)
+
 end
 
 dna = "TAATTAGAAATTCTATTATA"
-puts huffman_compress(dna)
+
+code = huffman_compress(dna)
+puts code
+
+decode = huffman_decompress(code)
+puts decode
